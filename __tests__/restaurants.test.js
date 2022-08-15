97b1f18cadbe91968ebc8aacc08e9bcff4e8bf74
Dillon Brock <mdillon1297@gmail.com>
2022-08-15 14:54:37 -0700

    wrote tests for review post route

--- __tests__/restaurants.test.js
+++ __tests__/restaurants.test.js
@@ -3,6 +3,15 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+const bob = {
+  firstName: 'Bob',
+  lastName: 'Belcher',
+  email: 'bobsburgers@email.com',
+  password: 'newbaconings',
+};
+
+const agent = request.agent(app);
+
 describe('restaurant and review routes', () => {
   beforeEach(() => {
     return setup(pool);
@@ -28,6 +37,30 @@ describe('restaurant and review routes', () => {
       city: 'Portland, OR',
       reviews: expect.any(Array),
     });
+  });
+  it('should add a new review if user is signed in', async () => {
+    const review = {
+      stars: 1,
+      description:
+        'I worked here for a year and it was the most misogynistic environment I ever experienced, which is saying something for the Portland food service scene.',
+    };
+    await agent.post('/api/v1/users').send(bob);
+    const res = await agent.post('/api/v1/users/4/reviews').send(review);
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      restaurant_id: '4',
+      user_id: expect.any(String),
+      ...review,
+    });
+  });
+  it('should give a 401 error if non-authenticated user tries to post review', async () => {
+    const review = {
+      stars: 4,
+      description: 'this will not be posted',
+    };
+    const res = await agent.post('/api/v1/users/3/reviews').send(review);
+    expect(res.status).toBe(401);
   });
   afterAll(() => {
     pool.end();
